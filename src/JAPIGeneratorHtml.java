9ed1a1d5c3c12a5538b6f54b25127f4b35e117da
itai-m <Itaimichael@gmail.com>
2015-06-26 12:19:28 +0300

    start the html view

---
+++ src/JAPIGeneratorHtml.java
@@ -0,0 +1,10 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class JAPIGeneratorHtml {
+	
+	public JAPIGeneratorHtml(String classname){
+		File myfile = new File(classname);
+	}
+}
